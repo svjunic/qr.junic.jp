8b46f1f9d7dc11b53400a30baf4ec6c1f5abead7
svjunic <sv.junic@gmail.com>
2020-01-22 15:03:28 +0900

    dialog追加

--- components/QRReader.vue
+++ components/QRReader.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <video id="stage" style="border: 1px solid gray"></video>
-    <v-btn small id="try" @click="clickHandler">読み取り</v-btn>
-    <v-text-field label="Main input" v-model="input" hide-details="auto"></v-text-field>
+    <v-btn small id="try" @click="clickHandler" v-show="!isFirstTime">読み取り</v-btn>
+    <!-- <v-text-field label="Main input" v-model="input" hide-details="auto"></v-text-field> -->
+    <v-dialog v-model="dialog" scrollable max-width="80%">
+      <v-card min-width="80vw" min-height="80vh">
+        <v-card-title>読み取り結果</v-card-title>
+        <v-card-text v-html="customInput"></v-card-text>
+        <v-btn color="green darken-1" text @click="dialog = false">閉じる</v-btn>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -10,30 +17,45 @@
 export default {
   data() {
     return {
+      dialog: false,
       input: '',
-      reader: null
+      reader: null,
+      isFirstTime: true
     };
   },
 
   async mounted() {
-    if (process.browser) {
-      this.$try = this.$el.querySelector('#try');
+    this.$try = this.$el.querySelector('#try');
 
-      const Reader = (await import('~/my_modules/reader.js')).default;
+    const Reader = (await import('~/my_modules/reader.js')).default;
 
-      this.$stage = document.querySelector('#stage');
-      this.reader = new Reader('stage');
-    }
+    this.$stage = document.querySelector('#stage');
+    this.reader = new Reader('stage');
+    this.read();
   },
 
   destroyed() {
     this.reader = null;
   },
 
+  computed: {
+    customInput() {
+      let customInput = this.input;
+      if (/^https?:\/\//.test(customInput)) {
+        customInput = `<a href="${customInput}">${customInput}</a>`;
+      }
+
+      return customInput;
+    }
+  },
+
   methods: {
     clickHandler(e) {
       console.log(this.reader);
       e.preventDefault();
+      this.read();
+    },
+    read() {
       this.reader
         .listen()
         .then(code => {
@@ -41,7 +63,10 @@ export default {
           this.input = code;
           this.$try.innerHTML = '再読み込み';
           this.$try.disabled = false;
-          this.$stage.hidden = true;
+          //this.$stage.hidden = true;
+
+          this.dialog = true;
+          this.isFirstTime = false;
         })
         .catch(e => {
           console.log(e);
@@ -50,3 +75,16 @@ export default {
   }
 };
 </script>
+
+<style lang="scss">
+#stage {
+  width: 100%;
+  height: 100%;
+}
+.dialog__slot {
+  &-content {
+    width: 80vw;
+    height: 60vh;
+  }
+}
+</style>
